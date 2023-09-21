58b73b64996e5553c6f20bc97f6e501eee3e59c5
Victim Musundire <mvpvictim@gmail.com>
2023-09-22 01:34:30 +0200

    dto model mapper

---
+++ src/main/java/com/victim/springboot/SpringbootRestfulWebservicesApplication.java
@@ -0,0 +1,22 @@
+package com.victim.springboot;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringbootRestfulWebservicesApplication {
+
+
+	// Defining Bean for ModelMapper
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestfulWebservicesApplication.class, args);
+	}
+
+}
